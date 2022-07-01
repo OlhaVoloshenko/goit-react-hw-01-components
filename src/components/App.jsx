a6f13fa436269d153b302e43c0e54c747ece7e0e
OlhaVoloshenko <voloshenko.olya@gmail.com>
2022-07-01 03:07:14 +0300

    222
    2222

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,26 @@
+import user from './user.json';
+import Profile from './Profile';
+
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      fontSize: 36,
+      color: '#052052'
+    }}
     >
-      React homework template
+    <Profile
+      username={user.username}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}
+    />
     </div>
   );
 };
+
+export default App;
