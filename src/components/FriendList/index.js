f99900c40492951e6dd2292cf36e23f451c45a69
OlhaVoloshenko <voloshenko.olya@gmail.com>
2022-07-03 21:48:30 +0300

    Update style and js Friends
    Update style and js Friends

--- src/components/FriendList/index.js
+++ src/components/FriendList/index.js
@@ -1,13 +1,31 @@
-export default function Friendlist({ friends }) {
+import propTypes from 'prop-types';
+import FriendListItem from '../FriendListItem';
+import style from './FriendList.module.css';
+
+export default function FriendList({ friends }) {
   return (
     <div>
-      <ul className="friend-list">
-        <li className="item">
-          <span className="status"></span>
-          <img className="avatar" src="" alt="User avatar" width="48" />
-          <p className="name"></p>
-        </li>
+      <ul className={style.friendList}>
+        {friends.map(friend => (
+          <FriendListItem
+            key={friend.id}
+            avatar={friend.avatar}
+            name={friend.name}
+            isOnline={friend.isOnline}
+          />
+        ))}
       </ul>
     </div>
   );
 }
+
+FriendList.propTypes = {
+  friends: propTypes.arrayOf(
+    propTypes.shape({
+      id: propTypes.number.isRequired,
+      avatar: propTypes.string.isRequired,
+      name: propTypes.string.isRequired,
+      isOnline: propTypes.bool.isRequired,
+    })
+  ),
+};
